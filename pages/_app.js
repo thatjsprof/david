0776ec0e3f1537bca9d16d084332d1410bc28fdb
davetech <davidrums65@gmail.com>
2024-02-03 22:07:44 +0100

    feat(app): add theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,34 @@
 import "../styles/global.css";
 import withMDX from "../hoc/withMDX";
 import Layout from "../components/layout";
+import { ThemeProvider } from "@mui/material/styles";
+import useColorProvider from "../hoc/colorProvider";
 
 function App({ Component, pageProps }) {
+  const { Provider, theme } = useColorProvider();
+
   return (
-    <Layout>
-      <div
-        style={{
-          width: "100%",
-        }}
-      >
-        {withMDX(pageProps)(Component)}
-      </div>
-    </Layout>
+    <Provider>
+      <ThemeProvider theme={theme}>
+        <div
+          style={{
+            color: theme.palette.mode === "dark" ? "#aaa" : "#000",
+            backgroundColor:
+              theme.palette.mode === "dark" ? "rgb(25, 25, 25)" : "#fff",
+          }}
+        >
+          <Layout>
+            <div
+              style={{
+                width: "100%",
+              }}
+            >
+              {withMDX(pageProps)(Component)}
+            </div>
+          </Layout>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
