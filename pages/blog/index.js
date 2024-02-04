8d1d5fce3d7475fa1f1f50f5d30905c2774ac659
davetech <davidrums65@gmail.com>
2024-02-04 21:24:23 +0100

    feat(blog): add hover state for blog tags

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,26 +1,85 @@
 import Link from "next/link";
 import utilStyles from "../../styles/utils.module.css";
-import { blogObject } from "../../constants/constants";
+import { blogObject, mainTags } from "../../constants/constants";
+import { useTheme } from "@mui/material";
+import { useState } from "react";
 
 const BlogPosts = () => {
+  const theme = useTheme();
+  const [currentTag, setCurrentTag] = useState("all");
+  const [hovered, setHovered] = useState({});
+
+  const handleMouseEnter = (slug) => {
+    setHovered((prev) => ({ ...prev, [slug]: true }));
+  };
+
+  const handleMouseLeave = (slug) => {
+    setHovered((prev) => ({ ...prev, [slug]: false }));
+  };
+
+  const handleClick = (slug) => {
+    setCurrentTag(slug);
+  };
+
   return (
     <div>
       <p
         style={{
           lineHeight: "1rem",
           letterSpacing: "0.02rem",
+          color: theme.palette.mode === "dark" ? "#aaa" : "#535353",
         }}
-        className={`${utilStyles.textColor} ${utilStyles.headingXl}`}
+        className={`${utilStyles.headingXl}`}
       >
         Blog Posts
       </p>
+      <div
+        style={{
+          marginTop: "2.5rem",
+          gap: ".8rem",
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        {mainTags(theme.palette.mode === "dark").map(
+          ({ title, slug, color, bgColor }) => {
+            return (
+              <div
+                key={slug}
+                onClick={() => handleClick(slug)}
+                onMouseEnter={() => handleMouseEnter(slug)}
+                onMouseLeave={() => handleMouseLeave(slug)}
+                style={{
+                  color,
+                  cursor: "pointer",
+                  padding: ".1rem 1rem",
+                  borderRadius: "7px",
+                  border: `1px solid ${color}`,
+                  backgroundColor:
+                    hovered?.[slug] || currentTag === slug
+                      ? bgColor
+                      : "transparent",
+                }}
+              >
+                {title}
+              </div>
+            );
+          }
+        )}
+      </div>
       <div
         style={{
           marginTop: "3rem",
         }}
       >
         {blogObject.map(({ year, data }) => {
-          return (
+          const validData =
+            currentTag === "all"
+              ? data
+              : data.filter((entry) => entry.tag === currentTag);
+
+          return validData.length > 0 ? (
             <div
               key={year}
               style={{
@@ -37,14 +96,14 @@ const BlogPosts = () => {
               >
                 {year}
               </p>
-              {data.map(({ date, title, link }) => {
+              {validData.map(({ date, title, link }) => {
                 return (
                   <div
                     key={title}
                     style={{
                       mb: 2,
                       display: "flex",
-                      columnGap: "1.5rem",
+                      columnGap: ".3rem",
                       alignItems: "start",
                       marginBottom: ".5rem",
                     }}
@@ -55,6 +114,7 @@ const BlogPosts = () => {
                         fontSize: "1.2rem",
                         marginBottom: "0rem",
                         whiteSpace: "nowrap",
+                        minWidth: "6rem",
                       }}
                     >
                       {date}
@@ -62,11 +122,12 @@ const BlogPosts = () => {
                     <Link href={link}>
                       <div
                         style={{
-                          color: "#535353",
+                          color:
+                            theme.palette.mode === "dark" ? "#aaa" : "#535353",
                           cursor: "pointer",
                           fontWeight: "bold",
-                          fontSize: "1.5rem",
-                          marginTop: "-.3rem",
+                          fontSize: "1.2rem",
+                          // marginTop: "-.3rem",
                         }}
                       >
                         {title}
@@ -76,6 +137,8 @@ const BlogPosts = () => {
                 );
               })}
             </div>
+          ) : (
+            <></>
           );
         })}
       </div>
